350a73dd3fba51e7eb1550d1d58af99c65c47fbd
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-04-24 09:52:30 -0500

    return json response

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    data = { 'Message': 'Hello World!' }
+    return JsonResponse(data)
